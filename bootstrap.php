dde84dac8825da82843778386f7afedad6e91098
Kaare Hoff Skovgaard <khs@secoya.dk>
2013-06-19 16:38:39 +0200

    Do not alias the rest client to the global

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,3 @@
 <?php
 
-Autoloader::add_core_namespace('Secoya\\Rest');
-
-Autoloader::add_classes(array(
-	'Secoya\\Rest\\Client' => __DIR__.'/classes/client.php',
-	'Secoya\\Rest\\RestException' => __DIR__.'/classes/restexception.php',
-	'Secoya\\Rest\\HttpException' => __DIR__.'/classes/httpexception.php',
-));
+Autoloader::add_namespace('Secoya\\Rest', __DIR__.'/classes/');
